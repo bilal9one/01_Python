d8dec44666fe2b3739aeb61fbd1d8bc7a209a17f
Bilal Maqsood <bilal.maqsood@rocketmail.com>
2017-01-31 16:23:13 +0500

    Add files via upload

---
+++ 01_01 HelloWorld.py
@@ -0,0 +1,11 @@
+# How to write your first program in Python. 
+# print "Hello World"
+
+def main():
+	print "Hello World !"
+
+# Below mentioned line(s) are for the purpose if one wants to include it as a library or execute it. If one includes a program as library with below mentioned if condition in it, the
+# program will not execute.
+
+if __name__ == "__main__" :
+	main()
